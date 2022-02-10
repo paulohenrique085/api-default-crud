2f9b15091422445e0bcddc9a25de8c7d8d48eb39
unknown <paulohnq55@outlook.com>
2022-02-09 21:23:31 -0300

    feat: creating database connection

--- src/databases/mongodb.js
+++ src/databases/mongodb.js
@@ -1,1 +1,21 @@
 //creating database connection
+
+const mongoose = require('mongoose')
+require('dotenv').config({ path: 'env/.env' })
+class MongoDB {
+    static createConnection() {
+        mongoose.connect(process.env.MONGO_DB, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        }, function(err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('MongoDB connected successfull !!')
+            }
+        })
+    }
+
+}
+
+module.exports = MongoDB
