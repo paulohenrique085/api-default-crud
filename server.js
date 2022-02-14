e00a1ef81c7a87314b80139b1b6fc71b408f757f
unknown <paulohnq55@outlook.com>
2022-02-14 20:52:27 -0300

    feature; add server

---
+++ server.js
@@ -0,0 +1,21 @@
+const mongodb = require('./src/databases/mongodb')
+
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const routes = require('./src/routes')
+
+const app = express()
+app.use(cookieParser())
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+//connecting to the database
+mongodb.createConnection()
+
+
+//going up server
+app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`)
+})
