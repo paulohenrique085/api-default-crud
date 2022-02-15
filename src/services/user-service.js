901ee0134f737e97ad3039071ca5f450d7500ee1
unknown <paulohnq55@outlook.com>
2022-02-14 22:03:19 -0300

    feature: creating services for user

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,1 +1,24 @@
 //creating services
+const User = require('../models/user')
+class UserService {
+
+    static async listUsers() {
+
+    }
+
+    static async createUser(event) {
+        await User.createUser(event)
+
+    }
+
+    static async listUserById() {
+
+    }
+
+    static async deleteUserByCpf() {
+
+    }
+
+}
+
+module.exports = UserService
