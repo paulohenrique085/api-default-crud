5b6fa40c7be61ec7a1d26edbcb877a080f73bbe8
unknown <paulohnq55@outlook.com>
2022-02-14 22:01:57 -0300

    feature: configuring handlers

--- src/handlers/user-handler.js
+++ src/handlers/user-handler.js
@@ -1,1 +1,31 @@
-//creating creating handlers
+const UserService = require('../services/user-service')
+
+class UserHandler {
+
+    static async listUsers(req, res) {
+
+    }
+
+    static async createUser(req, res) {
+        const user = req.body
+
+        try {
+            await UserService.createUser(user)
+            res.status(200).json({ message: "User registered successfully" })
+
+        } catch (error) {
+            res.status(400).json({ message: "Error when registering user" })
+        }
+
+    }
+
+    static async listUserById(req, res) {
+
+    }
+
+    static async deleteUserByCpf(req, res) {
+
+    }
+}
+
+module.exports = UserHandler
