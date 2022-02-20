f1cedc72b93199a5f41580082486565d6b84839c
Paulo Henrique Silva dos Santos <paulo.santos@mundiale.com.br>
2022-02-20 10:54:05 -0300

    feat: indenting code

--- src/databases/mongodb.js
+++ src/databases/mongodb.js
@@ -2,17 +2,19 @@
 const { resolve } = require('path')
 
 const mongoose = require('mongoose')
+
 require('dotenv').config({ path: resolve('env', '.env') })
+
 class MongoDB {
     static createConnection() {
         mongoose.connect(process.env.MONGO_DB, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
-        }, function(err) {
+        }, function (err) {
             if (err) {
                 console.log(err)
             } else {
-                console.log('MongoDB connected successfull !!')
+                console.log('Database connected successfull !!')
             }
         })
     }
