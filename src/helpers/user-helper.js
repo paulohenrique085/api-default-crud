b131318ad841f08fac430ebb72445e6e9faea2c6
Paulo Henrique Silva dos Santos <paulo.santos@mundiale.com.br>
2022-02-25 23:20:10 -0300

    feat: create user helper

--- src/helpers/user-helper.js
+++ src/helpers/user-helper.js
@@ -1,1 +1,17 @@
-//creating helpers
+const User = require('../models/user')
+
+class UserHelper {
+
+    static async checkUserInDatabase(cpf) {
+
+        const user = await User.checkUserExistence(cpf)
+
+        if (!user) {
+            return false
+        }
+        return true
+
+    }
+
+}
+module.exports = UserHelper
