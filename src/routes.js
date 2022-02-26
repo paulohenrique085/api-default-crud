c8be216533e1aff6d9571d78b8f1c44b1d2c3ae6
Paulo Henrique Silva dos Santos <paulo.santos@mundiale.com.br>
2022-02-26 17:38:45 -0300

    feat: creating novat route to update use

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,6 @@ const UserHandler = require('./handlers/user-handler')
 routes.post('/create-user', UserHandler.createUser)
 routes.get('/users', UserHandler.listUsers)
 routes.delete('/delete-user', UserHandler.deleteUserByCpf)
+routes.put('/update-user', UserHandler.updateUser)
+
 module.exports = routes
