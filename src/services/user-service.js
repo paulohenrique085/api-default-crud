1894f6eac582147ba235d0681c4ab5a094c41853
Paulo Henrique Silva dos Santos <paulo.santos@mundiale.com.br>
2022-02-26 17:41:03 -0300

    refactor: code refactor

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -3,18 +3,23 @@ const User = require('../models/user')
 
 class UserService {
 
-    static async createUser(credentials) {
-
-        await User.createUser(credentials)
+    static async createUser(user) {
+        await User.createUser(user)
     }
 
     static async listUsers() {
         const userList = await User.listUsers()
         return userList
     }
+
     static async deleteUserByCpf(cpf) {
         await User.deleteUserByCpf(cpf)
     }
+
+    static async updateUser(fullName, age, cpf) {
+        return User.updateUser(fullName, age, cpf)
+    }
+
 
 }
 
