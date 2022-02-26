1cc34a8ebe30756c60983c520ede6d8f9994dd10
Paulo Henrique Silva dos Santos <paulo.santos@mundiale.com.br>
2022-02-26 17:39:39 -0300

    feat: adicionando adding user update handler

--- src/handlers/user-handler.js
+++ src/handlers/user-handler.js
@@ -5,7 +5,6 @@ class UserHandler {
 
     static async createUser(req, res) {
         const credentials = req.body
-
         const { cpf } = req.body
 
         const userExistence = await UserHelper.checkUserInDatabase(cpf)
@@ -20,11 +19,9 @@ class UserHandler {
                 res.status(200).json({ message: "User registered successfully" })
 
             } catch (error) {
-                res.status(400).json({ message: "Error when registering user" })
+                res.status(500).json({ message: "Error when registering user" })
             }
         }
-
-
     }
 
 
@@ -36,7 +33,7 @@ class UserHandler {
 
         } catch (error) {
 
-            res.status(400).json({ message: "Error listing users" })
+            res.status(500).json({ message: "Error listing users" })
         }
 
     }
@@ -59,10 +56,31 @@ class UserHandler {
 
             } catch (error) {
 
-                res.status(400).json({ message: "Error deleting user" })
+                res.status(500).json({ message: "Error deleting user" })
 
             }
         }
+    }
+
+    static async updateUser(req, res) {
+        const { fullName, age, cpf } = req.body
+
+
+        const userExistence = await UserHelper.checkUserInDatabase(cpf)
+
+        if (!userExistence) {
+            res.status(400).json({ message: "The user does not exist in our database" })
+        }
+
+        else {
+            try {
+                await UserService.updateUser(fullName, age, cpf)
+                res.status(200).json("User successfully updated")
+            } catch (error) {
+                res.status(500).json({ message: "Error deleting user" })
+            }
+        }
+
     }
 
 
